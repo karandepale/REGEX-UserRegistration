4b5a52abac1a5762f7ce0de266969130a0518258
“karandepale” <karandepale111@gmail.com>
2023-06-08 14:07:42 +0530

    Add: Checking Mobile number

--- RegexUserRegistration/Program.cs
+++ RegexUserRegistration/Program.cs
@@ -16,11 +16,17 @@ namespace RegexUserRegistration
             Console.WriteLine("Enter Email address: ");
             string email = Console.ReadLine();
 
+            Console.WriteLine("Enter Mobile number: ");
+            string mobileNumber = Console.ReadLine();
+
             string namePattern = "^[A-Z][a-zA-Z]{2,}$";
             string emailPattern = @"^[a-zA-Z0-9]+([._+-][a-zA-Z0-9]+)*@[a-zA-Z0-9]+([.-][a-zA-Z0-9]+)*\.[a-zA-Z]{2,}$";
+            string mobilePattern = @"^\d{2}\s\d{10}$";
+
             bool isValidFirstName = Regex.IsMatch(firstName, namePattern);
             bool isValidLastName = Regex.IsMatch(lastName, namePattern);
             bool isValidEmail = Regex.IsMatch(email, emailPattern);
+            bool isValidMobileNumber = Regex.IsMatch(mobileNumber, mobilePattern);
 
             if (isValidFirstName)
             {
@@ -47,6 +53,15 @@ namespace RegexUserRegistration
             else
             {
                 Console.WriteLine("Invalid email address.");
+            }
+
+            if (isValidMobileNumber)
+            {
+                Console.WriteLine("Valid mobile number.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid mobile number.");
             }
         }
     }
