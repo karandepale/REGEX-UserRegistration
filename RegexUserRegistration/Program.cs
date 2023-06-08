bfd6c13cc94c793c88ea3790adead7f23037b364
“karandepale” <karandepale111@gmail.com>
2023-06-08 13:59:30 +0530

    Add: Check first name

---
+++ RegexUserRegistration/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace RegexUserRegistration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter First name: ");
+            string firstName = Console.ReadLine();
+
+            string pattern = "^[A-Z][a-zA-Z]{2,}$";
+            bool isValidFirstName = Regex.IsMatch(firstName, pattern);
+
+            if (isValidFirstName)
+            {
+                Console.WriteLine("Valid first name.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid first name.");
+            }
+        }
+    }
+}
